a938329cca3957cac75ea8d1bafb394f37b54b8c
neerajreddy9989 <naganeerajreddy9989@gmail,com>
2024-12-12 14:08:30 +0530

    games changes

--- src/sections/auth/ChessEmbed.tsx
+++ src/sections/auth/ChessEmbed.tsx
@@ -1,16 +1,14 @@
-const ChessEmbed = () => {
-    return (
-      <div style={{ position: 'relative', width: '100%', height: '500px' }}>
-        <iframe
-          src="/games/chess/index.html"
-          width="100%"
-          height="100%"
-          style={{ border: 'none' }}
-          sandbox="allow-scripts allow-same-origin"
-          title="Embedded Game"
-        ></iframe>
-      </div>
-    );
-  };
+const ChessEmbed = () => (
+  <div style={{ position: 'relative', width: '100%', height: '500px' }}>
+    <iframe
+      src="/games/chess/index.html"
+      width="100%"
+      height="100%"
+      style={{ border: 'none' }}
+      sandbox="allow-scripts allow-same-origin allow-modals"
+      title="Embedded Game"
+    />
+  </div>
+);
 
-  export default ChessEmbed;
+export default ChessEmbed;
--- src/sections/auth/GameEmbed.tsx
+++ src/sections/auth/GameEmbed.tsx
@@ -1,16 +1,14 @@
-const GameEmbed = () => {
-  return (
-    <div style={{ position: 'relative', width: '100%', height: '500px' }}>
-      <iframe
-        src="/games/sudoku/index.html"
-        width="100%"
-        height="100%"
-        style={{ border: 'none' }}
-        sandbox="allow-scripts allow-same-origin"
-        title="Embedded Game"
-      ></iframe>
-    </div>
-  );
-};
+const GameEmbed = () => (
+  <div style={{ position: 'relative', width: '100%', height: '500px' }}>
+    <iframe
+      src="/games/sudoku/index.html"
+      width="100%"
+      height="100%"
+      style={{ border: 'none' }}
+      sandbox="allow-scripts allow-same-origin"
+      title="Embedded Game"
+    />
+  </div>
+);
 
 export default GameEmbed;
